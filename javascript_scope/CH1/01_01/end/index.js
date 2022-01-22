f0513cb8971f5c85330eda1b799984e49460f327
yuanDataScience <huangyuan2000@hotmail.com>
2022-01-22 16:36:05 -0500

    complete javascript scope notebook

---
+++ javascript_scope/CH1/01_01/end/index.js
@@ -0,0 +1,17 @@
+var warrior = 'Ninja';
+
+const screamWarrior = () => {
+    let warrior2 = 'Samurai';
+    return {
+        shootWarrior: () => {
+            return console.log(warrior, warrior2);
+            
+        }
+    }
+}
+
+const newWarrior = screamWarrior();
+
+newWarrior.shootWarrior();
+
+// console.log(warrior, warrior2);
